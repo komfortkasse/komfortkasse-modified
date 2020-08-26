af09cb10dc0afc9b73ec99138931e2df0ceff034
machajdik <manuel.machajdik@gmail.com>
2020-08-26 14:31:06 +0200

    v1.4.4.14
    Compatibility for modified 2.0.5

--- callback/komfortkasse/install.php
+++ callback/komfortkasse/install.php
@@ -3,13 +3,12 @@
  * Komfortkasse
  * Installer
  *
- * @version 1.4.3-modified/xtc3
- * 
+ * @version 1.4.4.14-modified/xtc3
+ *
  * use these SQL statements to delete the configuration entries in order to re-install the plugin:
  * delete from configuration_group where configuration_group_title='Komfortkasse';
  * delete from configuration where configuration_key like 'KOMFORTKASSE%';
  */
-
 // error_reporting(E_ALL);
 // ini_set('display_errors', '1');
 ?>
@@ -28,9 +27,9 @@ Note: if the installer exits before step <?php echo $steps; ?> without an error 
 Including files...
 
 <?php
-$basepath = explode('callback', $_SERVER['SCRIPT_FILENAME']) ;
-require_once ($basepath[0].'includes/configure.php');
-require_once (DIR_WS_INCLUDES.'application_top_callback.php');
+$basepath = explode('callback', $_SERVER ['SCRIPT_FILENAME']);
+require_once ($basepath [0] . 'includes/configure.php');
+require_once (DIR_WS_INCLUDES . 'application_top_callback.php');
 require_once ('Komfortkasse_Config.php');
 ?>
 
@@ -44,9 +43,7 @@ $lng->get_browser_language();
 echo $lng->language ['code'];
 ?>
 
-<br />
-	<br />
-	<b><?php echo ++$step;?>/<?php echo $steps;?></b>
+<br /> <br /> <b><?php echo ++$step;?>/<?php echo $steps;?></b>
 Checking Language additions...
 
 <?php
@@ -69,9 +66,7 @@ if (!$found) {
 
 ?>
 
-<br />
-	<br />
-	<b><?php echo ++$step;?>/<?php echo $steps;?></b>
+<br /> <br /> <b><?php echo ++$step;?>/<?php echo $steps;?></b>
 Determining Configuration Group ID...
 
 <?php
@@ -87,11 +82,11 @@ if (!$update) {
     $config_group_q1 = xtc_db_query("SELECT max(configuration_group_id) as maxid FROM " . TABLE_CONFIGURATION_GROUP);
     $config_group_a1 = xtc_db_fetch_array($config_group_q1);
     $config_group_id1 = $config_group_a1 ['maxid'] + 1;
-    
+
     $config_group_q2 = xtc_db_query("SELECT max(configuration_group_id) as maxid FROM " . TABLE_CONFIGURATION);
     $config_group_a2 = xtc_db_fetch_array($config_group_q2);
     $config_group_id2 = $config_group_a2 ['maxid'] + 1;
-    
+
     $config_group_id = max($config_group_id1, $config_group_id2);
 }
 
@@ -101,8 +96,14 @@ echo $config_group_id;
 <br /> <br /> <b><?php echo ++$step;?>/<?php echo $steps;?></b>
 Checking Admin Menu...
 <?php
+$admin = defined('DIR_ADMIN') ? DIR_ADMIN : 'admin/';
+if (!defined('DIR_FS_ADMIN'))
+    define('DIR_FS_ADMIN', DIR_FS_CATALOG . $admin);
+if (!defined('DIR_WS_ADMIN'))
+    define('DIR_WS_ADMIN', DIR_WS_CATALOG . $admin);
+
 $found = 0;
-$file = DIR_FS_DOCUMENT_ROOT . 'admin/includes/column_left.php';
+$file = DIR_FS_ADMIN . 'includes/column_left.php';
 $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 foreach ($lines as $line) {
     if ((strstr($line, 'Komfortkasse') !== FALSE) && (strstr($line, 'gID=' . $config_group_id) !== FALSE)) {
@@ -115,19 +116,19 @@ if (!$found) {
     ?>
 	if (($_SESSION['customers_status']['customers_status_id'] == '0') && ($admin_access['configuration'] == '1')) echo '&lt;li&gt;&lt;a href="' . xtc_href_link(FILENAME_CONFIGURATION, 'gID=<?php echo $config_group_id; ?>', 'NONSSL') . '" class="menuBoxContentLink"> -Komfortkasse&lt;/a&gt;&lt;/li&gt;';
 <hr /> After you have added the line, please re-start the installer.<br />
-	<br />
+		<br />
 	<?php
     die();
 } else {
     echo "ok";
 }
 ?>
-  
+
 <br /> <br /> <b><?php echo ++$step;?>/<?php echo $steps;?></b>
 Creating Configuration Group...
 <?php
 if (!$update) {
-    $sql_data_array = array ('configuration_group_id' => $config_group_id,'configuration_group_title' => 'Komfortkasse','configuration_group_description' => 'Komfortkasse Konfiguration','sort_order' => $config_group_id,'visible' => 1 
+    $sql_data_array = array ('configuration_group_id' => $config_group_id,'configuration_group_title' => 'Komfortkasse','configuration_group_description' => 'Komfortkasse Konfiguration','sort_order' => $config_group_id,'visible' => 1
     );
     xtc_db_perform(TABLE_CONFIGURATION_GROUP, $sql_data_array);
 } else {
@@ -217,10 +218,9 @@ if ($ok) {
 
 ?>
 
-<br />
-	<br />
-	<b><?php echo ++$step;?>/<?php echo $steps;?></b> Finished. <a
-		href="<?php echo (ENABLE_SSL ? HTTPS_SERVER : HTTP_SERVER).DIR_WS_CATALOG?>admin/configuration.php?gID=<?php echo $config_group_id; ?>"
+<br /> <br /> <b><?php echo ++$step;?>/<?php echo $steps;?></b>
+		Finished. <a
+		href="<?php echo (ENABLE_SSL ? HTTPS_SERVER : HTTP_SERVER).DIR_WS_ADMIN?>configuration.php?gID=<?php echo $config_group_id; ?>"
 		target="_new">Please check the configuration now.</a><br /> (If you
 		cannot access this link, please login to your admin panel and open the
 		Komfortkasse configuration from the menu - should be the last menu
@@ -231,7 +231,9 @@ if ($ok) {
 		immediately. This way, your customer will receive payment information
 		immediately. We encourage you to activate instant order transmission.
 		In order to activate instant order transmission, edit the following
-		files:<br /> <br /> <b>/admin/orders.php</b>, around line 120: <pre>
+		files:<br /> <br /> <b><?php echo $admin ?>orders.php</b>, around line
+		120 (in newer versions of modified, the code is in file <b><?php echo $admin ?>includes/modules/orders_update.php</b>,
+		at the end of the file): <pre>
 xtc_db_perform(TABLE_ORDERS_STATUS_HISTORY,$sql_data_array);
 <b>
 // BEGIN Komfortkasse
@@ -270,7 +272,7 @@ define('MODULE_PAYMENT_EUTRANSFER_TEXT_DESCRIPTION', '&lt;br /&gt;After your ord
 </body>
 </html>
 
-<?php 
+<?php
 
 
 function insert_configuration($config_group_id, $config_key, $config_value, $use_function, $set_function, $sort_order)
@@ -279,13 +281,13 @@ function insert_configuration($config_group_id, $config_key, $config_value, $use
     $where = "configuration_group_id=" . $config_group_id . " and configuration_key='" . $config_key . "'";
     $check_s = xtc_db_query("SELECT * FROM " . TABLE_CONFIGURATION . " where " . $where);
     $check_a = xtc_db_fetch_array($check_s);
-    
+
     if ($check_a ['configuration_id']) {
-        $sql_data_array = array ('use_function' => $use_function,'set_function' => $set_function,'sort_order' => $sort_order 
+        $sql_data_array = array ('use_function' => $use_function,'set_function' => $set_function,'sort_order' => $sort_order
         );
         xtc_db_perform(TABLE_CONFIGURATION, $sql_data_array, 'update', $where);
     } else {
-        $sql_data_array = array ('configuration_group_id' => $config_group_id,'configuration_key' => $config_key,'configuration_value' => $config_value,'use_function' => $use_function,'set_function' => $set_function,'sort_order' => $sort_order 
+        $sql_data_array = array ('configuration_group_id' => $config_group_id,'configuration_key' => $config_key,'configuration_value' => $config_value,'use_function' => $use_function,'set_function' => $set_function,'sort_order' => $sort_order
         );
         xtc_db_perform(TABLE_CONFIGURATION, $sql_data_array);
     }
